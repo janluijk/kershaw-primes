6ba04a0cc868a4fdb7fb647b32ea04c36fff76a8
janluijk <luijk.jan@gmail.com>
2024-04-13 11:44:07 +0200

    Remove CPU code

--- main.cu
+++ main.cu
@@ -8,11 +8,10 @@
 
 // CPU
 std::vector<unsigned int> read_primes_from_file(const std::string &filename);
-std::vector<unsigned int> find_divisors(unsigned int n);
-unsigned int mod_exp(long long base, unsigned int exp, long long n);
 
 // GPU
-__global__ void kershaw_prime_kernel(unsigned int* primes, unsigned int* orders, std::size_t size);
+__global__ void kershaw_prime_kernel(unsigned int* primes, std::size_t size);
+__device unsigned int mod_exp(unsigned int base, unsigned int exp, unsigned int p);
 __device__ unsigned int compute_order(unsigned int p);
 __device__ unsigned int compute_base(unsigned int p, unsigned int order);
 __device__ bool compute_mod(unsigned int p, unsigned int order, unsigned int base);
@@ -28,42 +27,7 @@ int main(int argc, char *argv[]) {
   auto start = std::chrono::high_resolution_clock::now();
 
   std::vector<unsigned int> primes = read_primes_from_file(filename);
-  std::vector<std::pair<unsigned int, unsigned int>> prime_order; // Store results with prime number
   std::size_t num_primes = primes.size();
-
-  prime_order.reserve(num_primes * sizeof(std::pair<unsigned int, unsigned int>));
-
-  unsigned int num_threads = std::thread::hardware_concurrency();
-  std::size_t chunk_size = primes.size() / num_threads;
-  std::vector<std::vector<unsigned int>> chunks(num_threads);
-  for (unsigned int i = 0; i < num_threads; i++) {
-    auto start = primes.begin() + i * chunk_size;
-    auto end = (i == num_threads - 1) ? primes.end() : start + chunk_size;
-    chunks[i].assign(start, end);
-  }
-  std::mutex prime_order_mutex;
-  std::vector<std::thread> threads;
-  for (unsigned int i = 0; i < num_threads; i++) {
-    threads.emplace_back([&, i]() {
-      for (unsigned int prime : chunks[i]) {
-        std::vector<unsigned int> divisors = find_divisors(prime - 1);
-        unsigned int order;
-        for(unsigned int divisor : divisors) {
-          if (mod_exp(2, divisor, prime) == 1) {
-            order = divisor;
-            break;      
-          }
-        }
-        prime_order_mutex.lock();
-        prime_order.emplace_back(prime, order);
-        prime_order_mutex.unlock();
-      }
-    });
-  }
-
-  for (std::thread &t : threads) {
-    t.join();
-  }
 
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed_seconds = end - start;
@@ -73,24 +37,19 @@ int main(int argc, char *argv[]) {
   start = std::chrono::high_resolution_clock::now();
 
   unsigned int *d_primes;
-  unsigned int *d_orders;
 
   cudaMalloc(&d_primes, num_primes * sizeof(unsigned int));
-  cudaMalloc(&d_orders, num_primes * sizeof(unsigned int));
-
-  cudaMemcpy(d_primes, &prime_order[0].first, num_primes * sizeof(unsigned int), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_orders, &prime_order[0].second, num_primes * sizeof(unsigned int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_primes, primes.data(), num_primes * sizeof(unsigned int), cudaMemcpyHostToDevice);
 
   unsigned int block_size = 256;
   unsigned int num_blocks = (num_primes + block_size - 1) / block_size;
   start = std::chrono::high_resolution_clock::now();
   
-  kershaw_prime_kernel<<<num_blocks, block_size>>>(d_primes, d_orders, num_primes);
+  kershaw_prime_kernel<<<num_blocks, block_size>>>(d_primes, num_primes);
 
   cudaDeviceSynchronize();
 
   cudaFree(d_primes);
-  cudaFree(d_orders);
 
   end = std::chrono::high_resolution_clock::now();
   elapsed_seconds = end - start;
@@ -117,54 +76,43 @@ std::vector<unsigned int> read_primes_from_file(const std::string &filename) {
   return primes;
 }
 
-std::vector<unsigned int> find_divisors(unsigned int n) {
-  std::vector<unsigned int> divisors;
-  unsigned int sqrtN = sqrt(n);
-  for (unsigned int i = 1; i <= sqrtN; ++i) {
-    if (n % i == 0) {
-      divisors.push_back(i);
-      unsigned int inverse = n / i;
-      if (i != inverse) {
-        divisors.push_back(inverse);
-      }
-    }
-  }
-  std::sort(divisors.begin(), divisors.end());
-  return divisors;
-}
-
-unsigned int mod_exp(long long base, unsigned int exp, long long p) {
-  long long result = 1;
-
-  while (exp > 0) {
-    if (exp & 1) {
-      result = (result * base);
-      if (result > p)
-        result = result % p;
-    }
-    base = (base * base);
-    if (base > p)
-      base = base % p;
-    exp >>= 1;
-  }
-
-  return result;
-}
-
 // GPU
+__device__ unsigned int compute_order(unsigned int p) {
+  const unsigned int n = p - 1;
+  const unsigned int lim = sqrt(n);
+  unsigned int divisors[lim + 1];
+  int num_divisors = 0;
 
-// __device__ unsigned int compute_order(unsigned int p) {
-//   unsigned int val = 2, order = 1;
-//   while (val != 1) {
-//     val = val * 2;
-//     if (val > p) {
-//       val = val % p;
-//     }
-//     order++;
-//   }
+  for (unsigned int o = 1; i <= lim; ++i) {
+    if (n % i == 0) { // Found divisor
+      if (mod_exp(2, i, p) == 1)
+        return i;
+      
+      unsigned int inverse_divisor = n / i;
+      if (i != inverse_divisor)
+        divisors[num_divisors++] = inverse_divisor
+    }
+  }
 
-//   return order;
-// }
+  for (int j = num_divisors - 1; j >= 0; --j) {
+    if (mod_exp(2, divisors[j], p) == 1)
+      return divisors[j];
+  }
+
+  return -1;
+}
+
+__device unsigned int mod_exp(unsigned int base, unsigned int exp, unsigned int p) [
+  unsigned int result = 1;
+  base = base % p;
+  while (exp) {
+    if (exp & 1)
+      result = (result * base) % p;
+    
+    exp >>= 1;
+    base = (base * base) % p;
+  }
+}
 
 __device__ unsigned int compute_base(unsigned int p, unsigned int order) {
   unsigned long long result = 1;
